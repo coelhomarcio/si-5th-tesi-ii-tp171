249129098589ce69690d820360951ef3abd39726
Marcio Coelho <marciocoelho@gmail.com>
2021-11-26 21:57:43 -0300

    Updated app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
-import { NgModule }      from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-
-import { AppRoutingModule }        from "./app-routing.module";
-import { AppComponent }            from "./app.component";
+import { NgModule }                from "@angular/core";
+import { BrowserModule }           from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FormsModule }             from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AppComponent }         from "./app.component";
+import { CoelhoHomeComponent }  from "./component/coelho-home/coelho-home.component";
+import { CoelhoTasksComponent } from "./component/coelho-tasks/coelho-tasks.component";
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		CoelhoHomeComponent,
+		CoelhoTasksComponent
 	],
 	imports:      [
 		BrowserModule,
+		BrowserAnimationsModule,
 		AppRoutingModule,
-		BrowserAnimationsModule
+		FormsModule
 	],
 	providers:    [],
 	bootstrap:    [AppComponent]
