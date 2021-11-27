0dddca37a6a0b757dd7fcd3efdb4f3b860abe7a1
Marcio Coelho <marciocoelho@gmail.com>
2021-11-26 21:57:57 -0300

    Updated app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule }             from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { CoelhoHomeComponent }  from "./component/coelho-home/coelho-home.component";
+import { CoelhoTasksComponent } from "./component/coelho-tasks/coelho-tasks.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "home", component: CoelhoHomeComponent },
+	{ path: "tasks", component: CoelhoTasksComponent },
+	{ path: "", redirectTo: "home", pathMatch: "full" }
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
